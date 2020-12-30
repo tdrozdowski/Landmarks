47a8ae1473c43ef2a79b708abe23f5f63d243148
Terry Drozdowski <terry.drozdowski@icloud.com>
2020-12-29 22:50:39 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Terrence Drozdowski on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
