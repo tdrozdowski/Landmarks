a67470c46692dadf317455390ffa51795a2a2a69
Terry Drozdowski <terry.drozdowski@icloud.com>
2021-01-01 16:10:53 -0700

    completed UIKit section of tutorial

--- Views/PageView/PageView.swift
+++ Views/PageView/PageView.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 1
     
     var body: some View {
-        PageViewController(pages: pages)
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
     }
 }
 
